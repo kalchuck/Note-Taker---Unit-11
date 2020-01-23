7ca7da89bafd24f5f01ece67116f57b15da08719
Kalyan Gadda <kalchuck@gmail.com>
2020-01-22 22:04:02 -0700

    update the homework

---
+++ Note-Taker-master/server.js
@@ -0,0 +1,27 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+var fs = require('fs');
+var htmlRoutes = require("./routes/htmlRoutes")
+var apiRoutes = require("./routes/apiRoutes")
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 2007;
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use("/api", apiRoutes)
+app.use("/", htmlRoutes)
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
